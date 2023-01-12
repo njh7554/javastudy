a7bfbb0280a8a49cbb896d0c2322d25d0c95895c
njh7554 <njh7554@naver.com>
2023-01-12 12:39:20 +0900

    20230112

--- 04_Class/src/practice/MainClass.java
+++ 04_Class/src/practice/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
 		BankAccount myAcc = new BankAccount("010-8622-0710", 50_000);
 		BankAccount yourAcc = new BankAccount("010-1234-5678", 100_000);
 		
-		myAcc.transfer(yourAcc, 100_000);
+		myAcc.transfer(yourAcc, 50_000);
 		
 		myAcc.inquiry();
 		yourAcc.inquiry();
