7e6b6161885254ebfe24bb23ef6cf1b3f819228e
njh7554 <njh7554@naver.com>
2023-01-13 17:24:21 +0900

    20230113

---
+++ 05_Inheritance/src/pratice03_Bus/Person.java
@@ -0,0 +1,20 @@
+package pratice03_Bus;
+
+public class Person {
+
+	// 필드
+	private String name;
+
+	// 생성자
+	public Person(String name) {
+		this.name = name;
+	}
+
+	// 메소드
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ 05_Inheritance/src/pratice03_Bus/Student.java
@@ -0,0 +1,9 @@
+package pratice03_Bus;
+
+public class Student {
+
+	// 생성자
+	public Student(String name) {
+		
+	}
+}
